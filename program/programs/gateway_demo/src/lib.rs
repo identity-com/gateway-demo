af04d2f514734973a2af861c100180c24e490524
William Brooks <william@identity.org>
2022-02-15 22:51:21 +0200

    IDCOM-1811 Update program address

--- program/programs/gateway_demo/src/lib.rs
+++ program/programs/gateway_demo/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("Cq18woAS5xVXb6Ci24pxk6f1ptLx4h3HHcxm12LZPt1e");
 
 #[program]
 pub mod gateway_demo {
