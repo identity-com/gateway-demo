a43db385f534e1a197668d8715c75b033d4d5128
William Brooks <william@identity.org>
2022-03-01 11:40:45 +0200

    IDCOM-1851 Updated to build on tag

--- .github/deploy/frontend-deploy-site.sh
+++ .github/deploy/frontend-deploy-site.sh
@@ -3,16 +3,19 @@ set -e
 set -u
 
 FOLDER="/protected-transfer"
+SOURCE="./frontend/dist/"
 
 if [ "${STAGE}" == "prod" ]; then
-  DISTRIBUTION=E2S4Y0ZV7EAMWM
-  BUCKET=demo.identity.com
+  DISTRIBUTION=E23FRNHUJ372ZZ
+  BUCKET="demo-identity-com"
 elif [ ${STAGE} == "preprod" ]; then
   DISTRIBUTION=???
-  BUCKET=demo-preprod.identity.com
+  BUCKET=???
 elif [ ${STAGE} == "dev" ]; then
-  DISTRIBUTION=E1SU4WO39BJJ5E
-  BUCKET=demo-dev.identity.com
+  DISTRIBUTION=???
+  BUCKET=???
 fi
 
-npx deploy-aws-s3-cloudfront --non-interactive --react --bucket ${BUCKET} --destination ${FOLDER} --distribution ${DISTRIBUTION}
+echo $SOURCE
+
+npx deploy-aws-s3-cloudfront --non-interactive --react --source ${SOURCE} --bucket ${BUCKET} --destination ${FOLDER} --distribution ${DISTRIBUTION}
